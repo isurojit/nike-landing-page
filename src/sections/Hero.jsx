4fdf149c2bc398b81ed582a8db4748b00d85df09
isurojit <iamsurojit@hotmail.com>
2024-03-27 09:37:29 +0530

    hero section added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { shoes, statistics } from "../constant";
+import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collections
+        </p>
+        <h1 className="mt-30 font-palanquin text-8xl max-sm:text-[72px] max:sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike Shoes</span>
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover the latest styles, colors, and innovation form Nike for your
+          active life.
+        </p>
+        <Button lable="Shop Now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-400 bg-primary bg-hero bg-cover bg center">
+        <img
+          src={bigShoe1}
+          alt="shoe collection"
+          width={610}
+          height={500}
+          className="object-contain relative z-10"
+        />
+      </div>
+      <div>
+        {shoes.map((shoe) => (
+          <div key={shoe}>
+            <ShoeCard />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
